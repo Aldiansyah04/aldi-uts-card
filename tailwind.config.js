5dbc133540e32a5b3e58063fb19c170535d1a74c
Aldiansyah <145820884+Aldiansyah04@users.noreply.github.com>
2024-11-11 21:55:53 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily:{
+      playfair: ['Playfair Display', 'serif'],
+      GermaniaOne :['Germania One', 'system-ui']
+    }
+  },
+  plugins: [],
+}
